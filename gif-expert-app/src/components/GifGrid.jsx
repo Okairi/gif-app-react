6708d6b87d3c6daec3f36bb038205b7d57d2d0cd
Alessandro <alessandroRetuerto@hotmail.com>
2022-06-21 13:24:44 -0500

    search gif

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { getApiGifs } from "../helpers/getGifs";
 
 //
 export const GifGrid = ({ category }) => {
-  getApiGifs(category);
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getApiGifs(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
--- gif-expert-app/src/helpers/getGifs.js
+++ gif-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getApiGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=5d5klkjHkqNuQebA2oDM4aJr7Zli0A7k&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=5d5klkjHkqNuQebA2oDM4aJr7Zli0A7k&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
@@ -9,6 +9,5 @@ export const getApiGifs = async (category) => {
     url: img.images.downsized_medium.url,
   }));
 
-  console.log(gifs);
   return gifs;
 };
